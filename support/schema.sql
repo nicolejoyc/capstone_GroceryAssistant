602133efb78ee889904b8c297e99cc7f9513a151
Richard Valien <98352633+rvalien-student@users.noreply.github.com>
2022-05-03 11:34:58 -0500

    Add 'none' as id 0, in poroduct,category & brand

--- support/schema.sql
+++ support/schema.sql
@@ -135,6 +135,7 @@ CREATE TABLE Unit (
 );
 
 INSERT INTO Product (ProductId, UserId, Name) VALUES
+  (0, 0, 'None'),
   (2, 0, 'Apple'),
   (3, 0, 'Orange'),
   (4, 0, 'Banana'),
@@ -271,6 +272,7 @@ INSERT INTO Product (ProductId, UserId, Name) VALUES
 SELECT setval('product_productid_seq', (SELECT MAX(ProductId) FROM Product));
 
 INSERT INTO Category (CategoryId, UserId, Name) VALUES
+  (0, 0, 'None'),
   (2, 0, 'Fruit'),
   (3, 0, 'Dairy'),
   (4, 0, 'Canned Goods'),
@@ -305,6 +307,7 @@ INSERT INTO Category (CategoryId, UserId, Name) VALUES
 SELECT setval('category_categoryid_seq', (SELECT MAX(CategoryId) FROM Category));
 
 INSERT INTO Brand (BrandId, UserId, Name) VALUES
+  (0, 0, 'None'),
   (2, 0, 'Johnson & Johnson'),
   (3, 0, 'Dole'),
   (4, 0, 'Famous Daves');

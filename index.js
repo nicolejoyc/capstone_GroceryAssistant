9270f06d7f7b893cc9bead54dc8080cc07ad4495
Nicole Crumbaker <ncrumbaker@student.cvtc.edu>
2022-05-11 17:40:13 -0500

    [finishes #182128603] Let user know when there are no items saved

--- index.js
+++ index.js
@@ -355,7 +355,7 @@ express()
       const locals = {
         'preference': false,
         'table': 'brand',
-        'title': 'Brand',
+        'title': 'Brands',
         'items': (brands) ? brands.rows : null
       };
       res.render('pages/interface-1', locals);
